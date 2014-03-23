26984064264c7348d21d6084b0f4fa41cf3a83ed
rachellogie <rachel.logie1@gmail.com>
2014-03-23 15:53:00 -0600

    User can add and delete ideas from the list

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 
 
 class IdeaBox < Sinatra::Base
+  set :method_override, true
+
+  db = {}
 
   get '/' do
-    "boooooo!"
+    erb :index, :locals => {db: db}
+  end
+
+  post '/' do
+    title = params[:idea_title]
+    description = params[:idea_description]
+    db[title] = description
+    erb :index, :locals => {db: db}
+  end
+
+  delete '/' do
+    name = params[:idea_name]
+    if db.has_key? name
+      db.delete(name)
+    end
+    erb :index, :locals => {db: db}
   end
 
 
